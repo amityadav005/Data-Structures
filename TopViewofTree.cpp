982f29f3669e5d2eb02d8df4137bcb7baf1d6087
amityadav005 <amityadav005@ymail.com>
2017-05-03 20:13:40 +0530

    updated

--- TopViewofTree.cpp
+++ TopViewofTree.cpp
@@ -7,6 +7,7 @@ struct Node {
     int data;
     Node *left;
     Node *right;
+     int hd;
 };
 
 
@@ -14,6 +15,7 @@ Node *newNode(int data) {
     Node *temp = (Node *) malloc(sizeof(Node));
     temp->data = data;
     temp->left = temp->right = NULL;
+    temp->hd = 0;
     return temp;
 }
 
@@ -25,21 +27,31 @@ void inorder(Node *root) {
     inorder(root->right);
 }
 
-void top_view(Node *root, int min, int max, int level) {
+void top_view(Node *root, int min, int max) {
     if (root == NULL)
         return;
-
-
-    if (level == min) {
-        cout << root->data<<" ";
-        min = level-1;
-    } else if (level == max) {
-        cout << root->data<<" ";
-        max = level+1;
+    queue<Node* >q;
+    q.push(root);
+    cout<<root->data<<" ";
+    while(!q.empty()){
+        Node* t = q.front();
+        q.pop();
+        if (t->hd== min) {
+            cout << t->data<<" ";
+            min = t->hd-1;
+        } else if (t->hd == max) {
+            cout << t->data<<" ";
+            max = t->hd+1;
+        }
+        if(t->left){
+            t->left->hd = t->hd-1;
+            q.push(t->left);
+        }
+        if(t->right){
+            t->right->hd = t->hd+1;
+            q.push(t->right);
+        }
     }
-
-    top_view(root->left, min, max, level - 1);
-    top_view(root->right, min, max, level + 1);
 
 
 }
@@ -52,12 +64,14 @@ int main() {
     root->right = newNode(6);
     // root->left->left = newNode(8);
     root->left->right = newNode(-4);
+    root->left->right->right = newNode(11);
+    root->left->right->right->right = newNode(13);
+    root->left->right->right->right->right = newNode(15);
     root->left->right->left = newNode(22);
     root->left->right->left->left = newNode(44);
     root->left->right->left->left->left = newNode(66);
     root->right->left = newNode(7);
     root->right->right = newNode(5);
-    // cout<<root->data;
-    top_view(root, 0, 1, 0);
+    top_view(root, -1,1);
 
 }
